81844237555b685cfc30661be97f9dfd449d7bcc
moamenFathy <moamenmahmoud118@gmail.com>
2025-10-25 22:14:03 +0300

    feat: implement layout using Chakra UI Grid component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
-import { Button } from "@chakra-ui/react";
+import { Button, Grid, GridItem, Show } from "@chakra-ui/react";
 
 const App = () => {
-  return <Button>hi</Button>;
+  return (
+    <Grid
+      templateAreas={{
+        lg: `"nav nav" "aside main"`,
+        base: `"nav" "main"`,
+      }}
+    >
+      <GridItem area="nav" bgColor="coral">
+        Nav
+      </GridItem>
+      <GridItem
+        area="aside"
+        bgColor="gold"
+        display={{ base: "none", lg: "block" }}
+      >
+        aside
+      </GridItem>
+      <GridItem area="main" bgColor="dodgerblue">
+        main
+      </GridItem>
+    </Grid>
+  );
 };
 
 export default App;
